5e226df348d58b62395e2d54e3579dc360a87189
Phelps <phelpsmemo@gmail.com>
2021-10-13 15:18:49 -0300

    add IDUtils

--- setup.py
+++ setup.py
@@ -37,7 +37,8 @@ setup_requires = [
 install_requires = [
     'invenio-i18n>=1.2.0',
     'Flask-Discussion==0.1.1',
-    'pydash>=5.1.0'
+    'pydash>=5.1.0',
+    'IDUtils>=1.1.9'
 ]
 
 packages = find_packages()
