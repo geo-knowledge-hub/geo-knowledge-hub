baa251e90116fcec98b084229ac46d8ee36dd93e
Felipe Carlos <efelipecarlos@gmail.com>
2022-04-29 07:51:39 -0300

    landing page: adding initial support for geospatial metadata visualizer

--- geo_knowledge_hub/modules/detail/webpack.py
+++ geo_knowledge_hub/modules/detail/webpack.py
@@ -21,9 +21,15 @@ theme = WebpackThemeBundle(
                 "geo-knowledge-hub-landing-user-stories": "./js/geo_knowledge_hub_detail/user_stories/index.js",
                 # Engagement carousel
                 "geo-knowledge-hub-landing-engagements-carousel": "./js/geo_knowledge_hub_detail/engagements_carousel_sidebar/index.js",
-                "geo-knowledge-hub-landing-engagements-carousel-theme": "./less/geo_knowledge_hub_detail/engagements_carousel_sidebar/theme.less"
+                "geo-knowledge-hub-landing-engagements-carousel-theme": "./less/geo_knowledge_hub_detail/engagements_carousel_sidebar/theme.less",
+                # Geospatial metadata visualizer
+                "geo-knowledge-hub-landing-geospatial-metadata-visualizer": "./js/geo_knowledge_hub_detail/geospatial_previewer/index.js",
+                "geo-knowledge-hub-landing-geospatial-metadata-visualizer-theme": "./less/geo_knowledge_hub_detail/geospatial_previewer/theme.less",
             },
             dependencies={
+                "leaflet": "^1.7.1",
+                "react-leaflet": "3.1.0",
+                "@react-leaflet/core": "1.0.2",
                 "natsort": "^2.0.3",
                 "lodash": "^4.17.0",
                 "react-glider": "^3.0.1",
