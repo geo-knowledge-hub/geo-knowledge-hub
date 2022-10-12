6c5ec3b4187d0f3a88b3f22effe60f59ade4417f
Felipe Carlos <efelipecarlos@gmail.com>
2022-10-11 21:41:13 -0300

    communities: fixing new community component and changing permission policy (Closes #150, Closes #152)

---
+++ geo_knowledge_hub/permissions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 GEO Secretariat.
+#
+# geo-knowledge-hub is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""GEO Knowledge Hub permissions module."""
+
+from geo_config.security.generators import GeoSecretariat
+from invenio_communities.permissions import CommunityPermissionPolicy
+from invenio_records_permissions.generators import SystemProcess
+
+
+class GEOCommunityPermissionPolicy(CommunityPermissionPolicy):
+    """Permissions for Community CRUD operations."""
+
+    can_create = [GeoSecretariat(), SystemProcess()]
