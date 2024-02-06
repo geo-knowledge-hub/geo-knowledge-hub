7e66f4256b7fc0378d2e53f04cc7eacdf30cf361
Felipe Carlos <efelipecarlos@gmail.com>
2024-02-06 09:54:16 -0300

    tests: updating run-tests.sh file

--- run-tests.sh
+++ run-tests.sh
@@ -9,7 +9,14 @@
 
 
 # Usage:
-#   env DB=postgresql12 SEARCH=elasticsearch7 CACHE=redis MQ=rabbitmq ./run-tests.sh
+#   ./run-tests.sh [-K|--keep-services] [pytest options and args...]
+#
+# Note: the DB, SEARCH and MQ services to use are determined by corresponding environment
+#       variables if they are set -- otherwise, the following defaults are used:
+#       DB=postgresql, SEARCH=opensearch and MQ=redis
+#
+# Example for using mysql instead of postgresql:
+#    DB=mysql ./run-tests.sh
 
 # Quit on errors
 set -o errexit
@@ -17,17 +24,40 @@ set -o errexit
 # Quit on unbound symbols
 set -o nounset
 
-# Always bring down docker services
-function cleanup() {
-    eval "$(docker-services-cli down --env)"
+# Define function for bringing down services
+function cleanup {
+  eval "$(docker-services-cli down --env)"
 }
-trap cleanup EXIT
 
-python -m check_manifest --ignore ".*-requirements.txt"
+# Check for arguments
+# Note: "-k" would clash with "pytest"
+keep_services=0
+pytest_args=()
+for arg in $@; do
+	# from the CLI args, filter out some known values and forward the rest to "pytest"
+	# note: we don't use "getopts" here b/c of some limitations (e.g. long options),
+	#       which means that we can't combine short options (e.g. "./run-tests -Kk pattern")
+	case ${arg} in
+		-K|--keep-services)
+			keep_services=1
+			;;
+		*)
+			pytest_args+=( ${arg} )
+			;;
+	esac
+done
+
+if [[ ${keep_services} -eq 0 ]]; then
+	trap cleanup EXIT
+fi
+
+export LC_TIME=en_US.UTF-8
+python -m check_manifest
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
-# TODO: Remove services below that are not neeed (fix also the usage note).
-eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env)"
-python -m pytest
+eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --mq ${MQ:-redis} --env)"
+
+# Note: expansion of pytest_args looks like below to not cause an unbound
+# variable error when 1) "nounset" and 2) the array is empty.
+python -m pytest ${pytest_args[@]+"${pytest_args[@]}"}
 tests_exit_code=$?
-python -m sphinx.cmd.build -qnNW -b doctest docs docs/_build/doctest
 exit "$tests_exit_code"
