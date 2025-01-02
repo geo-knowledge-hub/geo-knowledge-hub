d1e35afaefbdf4f4ef96c78e452cf7d362df98e8
Felipe Carlos <efelipecarlos@gmail.com>
2025-01-02 07:58:30 -0300

    site: fix events sort

--- site/geo_knowledge_hub/config.py
+++ site/geo_knowledge_hub/config.py
@@ -22,7 +22,7 @@ GKH_CAROUSEL_CONVENTIONS_URL = "/search?q=metadata.engagement_priorities.id"
 GKH_LATEST_RECORDS_URL = "/api/packages?sort=newest&size=3"
 GKH_LATEST_RECORDS_MORE_URL = "/search?f=record_type:package"
 
-GKH_LATEST_EVENTS_URL = "https://gkhub.earthobservations.org:8443/api/events?sort[0]=date&pagination[limit]=3"
+GKH_LATEST_EVENTS_URL = "https://gkhub.earthobservations.org:8443/api/events?sort[0]=date:desc&pagination[limit]=3"
 GKH_LATEST_EVENTS_MORE_URL = "https://gkhub.earthobservations.org/doc/events"
 
 # GEO Components React (Commercial solutions)
